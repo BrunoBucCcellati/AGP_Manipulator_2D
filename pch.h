7d988f060ae6343885dd084e9e61530128c5a19b
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2026-07-11 14:07:18 +0300

    Add files via upload

---
+++ pch.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#define _ENABLE_EXTENDED_ALIGNED_STORAGE
+
+#pragma inline_depth(255)
+#pragma managed(push, off)
+
+#include <oneapi/tbb/enumerable_thread_specific.h>
+#include <oneapi/tbb/parallel_invoke.h>
+
+#include <pybind11/embed.h>
+#include <pybind11/stl.h>
+
+#include <boost/align/aligned_allocator.hpp>
+#include <boost/mpi.hpp>

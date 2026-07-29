27a0498d7033b672aa1351e38dfec0d1e0f07a68
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2026-07-30 00:36:40 +0300

    Add deque include to pch.h

--- pch.h
+++ pch.h
@@ -1,15 +1,14 @@
-﻿#pragma once
+#pragma once
 
 #define _ENABLE_EXTENDED_ALIGNED_STORAGE
 
 #pragma inline_depth(255)
 #pragma managed(push, off)
 
+#include <deque>
+
 #include <oneapi/tbb/enumerable_thread_specific.h>
 #include <oneapi/tbb/parallel_invoke.h>
-
-#include <pybind11/embed.h>
-#include <pybind11/stl.h>
 
 #include <boost/align/aligned_allocator.hpp>
 #include <boost/mpi.hpp>
